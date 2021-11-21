2002f4dec9207ce2ccd14c9e33201766b780d15c
lhqlhhh <lhqlhhh@hotmail.com>
2021-11-21 15:31:50 +0800

    fix pic format problem

--- farfetch/__init__.py
+++ farfetch/__init__.py
@@ -6,19 +6,36 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import time
+import datetime
+from PIL import Image as PILImage
 
 
-def _save_image(url, header):
+def _save_image(url, header, path):
     res = requests.get(url, headers=header)
     file_name = url.split('/')[-1]
     #file_name = file_name[:-4] + ".webp"
-    if os.path.exists(file_name):
-        return file_name
+    file_name = file_name[:-4] + ".png"
+    file_path = path + "/" + file_name
+    if os.path.exists(file_path):
+        return file_path
     else:
-        with open(file_name, "wb") as f:
+        with open(file_path, "wb") as f:
             f.write(res.content)
 
-    return file_name
+    img = PILImage.open(file_path)
+    #print(img.format)
+    img.save(file_path, 'PNG')
+    #print(img.format)
+
+    return file_path
+
+
+def _current_year_mon():
+    time_format = '%Y-%m'
+    cur_year_month = datetime.datetime.now().strftime(time_format)
+    print(cur_year_month)
+
+    return cur_year_month
 
 
 class Farfetch:
@@ -32,6 +49,12 @@ class Farfetch:
         self.page_num = 1
         self.prod_pic_list = [[]]
         self.product_order = 0
+        #self.year_month = _current_year_mon()
+        self.path = "./farfetch_" + self.brand + "_" + _current_year_mon()
+        if not os.path.isdir(self.path):
+            pwd = os.getcwd()
+            print(pwd)
+            os.mkdir(pwd + self.path)
         # create workbook
         wb = Workbook()
         ws = wb.active
@@ -40,10 +63,10 @@ class Farfetch:
         self.ws.column_dimensions['A'].width = 25
         self.ws.column_dimensions['B'].width = 65
         self.rows = 0
-        self.excel_name = "farfetch_" + self.brand + ".xlsx"
+        self.excel_path = self.path + ".xlsx"
         # 判断excel是否已经存在, 已存在的情况下, 从断点开始
-        if os.path.exists(self.excel_name):
-            self.wb = load_workbook(self.excel_name)
+        if os.path.exists(self.excel_path):
+            self.wb = load_workbook(self.excel_path)
             self.ws = self.wb.get_sheet_by_name('Sheet')
             self.rows = self.ws.max_row    # 获取行数
             # 获取已完成的页数 + 1 : 未完成的第一页
@@ -89,7 +112,7 @@ class Farfetch:
         col = 3
         for pic in pics:
             # download pic
-            img_path = _save_image(str(pic["href"]), self.header)
+            img_path = _save_image(str(pic["href"]), self.header, self.path)
             #print(img_path)
 
             img = Image(img_path)
@@ -102,7 +125,7 @@ class Farfetch:
             self.ws.add_image(img, coor)
         # 下一个空行
         self.rows += 1
-        self.wb.save(self.excel_name)
+        self.wb.save(self.excel_path)
 
     def _browse_all_product(self, soup):
         self._list_products(soup)
@@ -117,7 +140,7 @@ class Farfetch:
         # reset
         self.prod_pic_list = [[]]
         # 保存到excel中
-        self.wb.save(self.excel_name)
+        self.wb.save(self.excel_path)
 
     def _make_soup(self, url):
         res = requests.get(url=url, headers=self.header)
@@ -126,6 +149,7 @@ class Farfetch:
 
     def shopping(self):
         while True:
+            print(self.url + "?page=" + str(self.page_num))
             if self.page_num == 1:
                 self._browse_all_product(self.first_page)
             else:
